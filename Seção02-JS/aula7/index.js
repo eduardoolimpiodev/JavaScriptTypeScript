ff022d532874cdf06878b5055cb29178ae4ce4b5
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-13 23:53:26 -0300

    Add: Numbers

--- Seção02-JS/aula7/index.js
+++ Seção02-JS/aula7/index.js
@@ -10,3 +10,9 @@ console.log("teste01, 'teste02'")
 console.log('teste01, "teste02"')
 console.log('"teste01", "teste02"');
 console.log(`'Teste01', "teste02"`);
+
+//Numbers
+console.log(123456);
+console.log(123456, 123.56);
+
+console.log(36, 12.58, "teste" );
