758234231172a6c0aa44f07edc2bdabbf9e2375a
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-13 23:51:06 -0300

    Add: String Types

---
+++ Seção02-JS/aula7/index.js
@@ -0,0 +1,12 @@
+
+//String type
+console.log("teste");
+console.log("teste")
+console.log('teste');
+console.log('teste')
+console.log(`teste`);
+console.log(`teste`)
+console.log("teste01, 'teste02'")
+console.log('teste01, "teste02"')
+console.log('"teste01", "teste02"');
+console.log(`'Teste01', "teste02"`);
