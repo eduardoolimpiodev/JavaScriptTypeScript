38fa9e86cc1c3a54631a277e8bb0456f275c6c99
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-14 00:13:59 -0300

    Add: Exercício

--- Seção02-JS/aula7/exercicio/exercicio.js
+++ Seção02-JS/aula7/exercicio/exercicio.js
@@ -1,1 +1,3 @@
 console.log('Meu nome é "Eduardo Olimpio". Estou aprendendo javaScript às 10 da manhã.');
+
+console.log('Meu nome é "Eduardo Olimpio". Estou aprendendo javaScript às', 10, 'da manhã.');
