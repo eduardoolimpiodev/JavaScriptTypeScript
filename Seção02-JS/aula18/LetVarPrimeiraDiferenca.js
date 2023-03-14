7d094b8fbba8348fe6e1f2d08328d908bb1241f7
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-14 18:18:04 -0300

    Add: Primeira Diferença entre VAR e LET

---
+++ Seção02-JS/aula18/LetVarPrimeiraDiferenca.js
@@ -0,0 +1,22 @@
+// Diferenças entre var e let - ECMAScript 2015 (ES6)
+
+var nome = 'Eduardo';
+var nome = 'Olimpio';
+
+console.log(nome);
+
+/* O mesmo código acima gera um erro se usarmos LET, pois não podemos reatrubuir um novo valor dire
+tamente. 
+*/
+
+//TENÇÃO: NÃO POSSO CRIAR VARIÁVEIS SEM A PALAVRA CONST OU LET.
+
+// EXEMPLO: 
+// nome = 'Eduardo';
+
+
+
+
+
+
+
