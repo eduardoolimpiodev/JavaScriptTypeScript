f249dacd128bda4248c3215be2ce0925e1241f99
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-14 12:22:52 -0300

    Add: Comentários

---
+++ Seção02-JS/aula11/comentarios.js
@@ -0,0 +1,13 @@
+// Comentário - barra dupla a engine do java script ignora a linha.
+console.log('Olá JavaScript');
+
+
+
+/* Outro tipo de comentário */
+
+/*
+
+Tudas as linhas dentro desse bloco serão ignroadas pela engine do JavaScript.
+
+
+*/
