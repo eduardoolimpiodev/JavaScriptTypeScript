c130632b82a191932da2fb6f926f7eef01733c8a
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-15 15:12:26 -0300

    Add: Exercícios de Numbers e Math

---
+++ Seção02-JS/aula28/ExNumbersMath.js
@@ -0,0 +1,19 @@
+const numero = Number(prompt('Digite um número'));
+
+const numeroTitulo = document.getElementById('numero-titulo');
+
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `
+<strong>
+<p>Seu número - 2 é:${numero - 2}.</p>
+<p>Seu número + 2 é: ${numero + 2}.</p>
+<p>Seu número é inteiro ? ${Number.isInteger(numero)}</P>
+<p>Raiz quadrada:${Math.sqrt(numero)}.</p>
+<p>É NaN ? ${Number.isNaN(numero)}.</p>
+<p>Arredondado para baixo: ${Math.floor(numero)}.</p>
+<p>Arredondado para cima: ${Math.ceil(numero)}.</p>
+<p>Com duas casas decimais: ${numero.toFixed(2)}</p>
+</strong>
+`;
