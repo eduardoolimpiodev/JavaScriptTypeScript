7c4fb43df2626264a668e2b92a68638133b035ba
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-14 21:42:02 -0300

    Add: Exercício.

---
+++ Seção02-JS/aula23/script.js
@@ -0,0 +1,15 @@
+//EXERCÍCIO
+
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+// const varATemporaria = varA;
+// varA = varB;
+// varB = varC;
+// varC = varATemporaria;
+
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);
+
